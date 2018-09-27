bfcb17fb5dea631f92408e4f1715b38165087957
Silvia Valdez <silvia.valdez.e@gmail.com>
2018-09-27 17:08:37 -0600

    SampleApp: WIP: Show activity indicator.

--- SampleApp/SampleApp/SignInViewController.swift
+++ SampleApp/SampleApp/SignInViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var buttonSignInView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Hide navigation bar
         self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+//        buttonSignInView.addTarget(self, action: Selector(("signIn:")), for: UIControlEvents.touchUpInside)
+
+        // Hide the activity indicator until button is clicked
+        // self.showActivityIndicator(show: false)
     }
+    
+    // @IBAction func signIn(sender: UIButton) {
+        // print("Clicked!")
+        // self.showActivityIndicator(show: true)
+
+        // DispatchQueue.main.asyncAfter(deadline: .now() + 2) { // 2: Number of seconds
+        // }
+    // }
+    
+//    func showActivityIndicator(show: Bool) {
+//        activityIndicatorView.isHidden = show
+//    }
     
 }
